7f4d18f57eb2825795d0966d1a70e686928923ad
ThusharaSamaraweera <thusharadasun204@gmail.com>
2023-04-08 21:55:37 +0530

    add base and error interceptors

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,8 @@ import { TranslocoRootModule } from './services/transloco-root.module';
 import { TranslocoLocaleModule } from '@ngneat/transloco-locale';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { APIInterceptor } from './interceptors/base-interceptor';
+import { ErrorInterceptor } from './interceptors/error-interceptor';
 
 const EXPORT_MODULES = [
   TranslocoRootModule,
@@ -15,6 +17,17 @@ const EXPORT_MODULES = [
   declarations: [],
   imports: [CommonModule, ReactiveFormsModule],
   exports: EXPORT_MODULES,
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: APIInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true,
+    },
+  ],
 })
 export class CoreModule {}
